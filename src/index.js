3e5c42a4352257ba3e0c327bcca26fc56f824387
Duncan Bain <duncan@bain.io>
2020-03-08 13:22:28 +0000

    added endpoints for hourly and daily

--- src/index.js
+++ src/index.js
@@ -10,10 +10,25 @@ app.listen(process.env.PORT, () =>
   console.log(`server started on port ${process.env.PORT}`)
 );
 
-app.get('/getweather/:latitude,:longitude', async (req, res) => {
+app.get('/weather/daily/:latitude,:longitude', async (req, res) => {
   try {
     const { latitude, longitude } = req.params;
-    const forecast =  await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKYAPIKEY}/${latitude},${longitude}`);
+    const forecast = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARKSKYAPIKEY}/${latitude},${longitude}?exclude=minutely,hourly&units=uk2`
+    );
+    const forecastJson = await forecast.json();
+    res.json(forecastJson);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/weather/hourly/:latitude,:longitude', async (req, res) => {
+  try {
+    const { latitude, longitude } = req.params;
+    const forecast = await fetch(
+      `https://api.darksky.net/forecast/${process.env.DARKSKYAPIKEY}/${latitude},${longitude}?exclude=minutely&units=uk2`
+    );
     const forecastJson = await forecast.json();
     res.json(forecastJson);
   } catch (error) {
